08f1096b13a36694757f9df998b1b9c2dc0d8e23
devltt404 <phamductri4862@gmail.com>
2023-01-10 23:29:55 +0700

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function open_card() {
   document.getElementById("front").className = "open-card";
 }
+function open_card2() {
+  document.getElementById("inside").className = "open-card";
+}
+function open_card3() {
+  document.getElementById("instruct1").className = "open-card";
+}
 
 function close_card() {
   document.getElementById("front").className = "";
+}
+
+function close_card2() {
+  document.getElementById("inside").className = "";
+}
+function close_card3() {
+  document.getElementById("instruct1").className = "";
 }
